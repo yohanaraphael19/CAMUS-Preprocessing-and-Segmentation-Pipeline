a54cc37c16cd8d9df3b19f9b9cc09479e6a0437d
Yohana Raphael <yohanaraphael19@gmail.com>
2025-01-27 18:34:05 +0800

    Update train_validation.py

--- train_validation.py
+++ train_validation.py
@@ -1,48 +1,51 @@
 import torch
 from tqdm import tqdm
-
 from utils import dice_coefficient
+
+class DiceBCELoss(nn.Module):
+    def __init__(self):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, pred, target):
+        bce = torch.nn.functional.binary_cross_entropy(pred, target)
+        smooth = 1e-6
+        pred_flat = pred.view(-1)
+        target_flat = target.view(-1)
+        intersection = (pred_flat * target_flat).sum()
+        dice_loss = 1 - (2. * intersection + smooth) / (pred_flat.sum() + target_flat.sum() + smooth)
+        return bce + dice_loss
 
 def train(model, train_loader, optimizer, criterion, device):
     model.train()
     running_loss = 0.0
-
-    for images, masks in tqdm(train_loader, desc="Training", leave=False):
+    for images, masks in tqdm(train_loader, desc="Training"):
         images, masks = images.to(device), masks.to(device)
-
-        masks = (masks > 0.5).float()  # Convert to binary (0 or 1)
-
+        
         optimizer.zero_grad()
         outputs = model(images)
         loss = criterion(outputs, masks)
         loss.backward()
         optimizer.step()
-
+        
         running_loss += loss.item()
-    
     return running_loss / len(train_loader)
-
 
 def validate(model, val_loader, criterion, device):
     model.eval()
     running_loss = 0.0
     running_dice = 0.0
-
+    
     with torch.no_grad():
-        for images, masks in tqdm(val_loader, desc="Validating", leave=False):
+        for images, masks in tqdm(val_loader, desc="Validating"):
             images, masks = images.to(device), masks.to(device)
-
-            masks = (masks > 0.5).float() 
-
             outputs = model(images)
             loss = criterion(outputs, masks)
+            
+            # Calculate Dice
+            outputs_bin = (outputs > 0.5).float()
+            dice = dice_coefficient(outputs_bin.cpu().numpy(), masks.cpu().numpy())
+            
             running_loss += loss.item()
-
-            # Compute dice coefficient
-            outputs = (outputs > 0.5).float()
-            dice = dice_coefficient(masks.cpu().numpy(), outputs.cpu().numpy())
             running_dice += dice
     
-    avg_loss = running_loss / len(val_loader)
-    avg_dice = running_dice / len(val_loader)
-    return avg_loss, avg_dice
+    return running_loss / len(val_loader), running_dice / len(val_loader)
